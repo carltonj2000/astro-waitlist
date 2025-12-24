3c62b819624bc5b0d0d616719a665e230e957e7b
Carlton Joseph <carlton.joseph@gmail.com>
2025-12-23 19:42:35 -0800

    add test with throws

--- src/server/db/queries.test.ts
+++ src/server/db/queries.test.ts
@@ -1,15 +1,36 @@
 import { it, expect, mock, beforeEach } from "bun:test";
-import { insertSubscriber } from "./queries";
 import type { D1Database } from "@cloudflare/workers-types";
+import { reset } from "drizzle-seed";
+
+import { insertSubscriber } from "./queries";
 import type { NewSubscriber } from "./schema";
+import * as schema from "./schema";
 import { getTestDb } from "../../../test/get-test-db";
 
 mock.module("./db.ts", () => {
   return { getDb: () => getTestDb() };
 });
 
+beforeEach(async () => {
+  const db = getTestDb();
+  await reset(db, schema);
+});
+
 it("insert a new subscriber in the db", async () => {
   const newSub: NewSubscriber = { email: "test@test.com" };
   const subscriber = await insertSubscriber({} as D1Database, newSub);
-  console.log(subscriber);
+  expect(subscriber.email).toBe(newSub.email);
+  expect(subscriber.id).toBeDefined();
+  expect(subscriber.createdAt).toBeDefined();
+});
+
+it("throws error for duplicate email", async () => {
+  const newSub: NewSubscriber = { email: "test@test.com" };
+  await insertSubscriber({} as D1Database, newSub);
+  expect(insertSubscriber({} as D1Database, newSub)).rejects.toThrow();
+});
+
+it("throws error for invalid email", async () => {
+  const newSub: NewSubscriber = { email: "testtest.com" };
+  expect(insertSubscriber({} as D1Database, newSub)).rejects.toThrow();
 });
